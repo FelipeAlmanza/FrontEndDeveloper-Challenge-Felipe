8a4a1f441271b4693abd4599fa6044868808e22a
FelipeAlmanza <FelipeAlmanza@users.noreply.github.com>
2021-02-22 13:57:25 -0600

    Fixed typo

--- js/loadElements.js
+++ js/loadElements.js
@@ -1,4 +1,4 @@
-// Load shared elements of pages
+// Loads shared elements of pages
 $("#navPlaceholder").load("nav-header.html");
 $("#footerPlaceHolder").load("footer.html");
 $("searchBoxContainer").load("searchBox.html");
